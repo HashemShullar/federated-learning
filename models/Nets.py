4246bb31471db62ecc5477067448e8c89228a275
HashemShullar <86099636+HashemShullar@users.noreply.github.com>
2022-08-13 23:45:12 +0300

    Add files via upload

--- models/Nets.py
+++ models/Nets.py
@@ -61,3 +61,61 @@ class CNNCifar(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+class CNNHeart(nn.Module):
+    def __init__(self, args):
+        super(CNNHeart, self).__init__()
+
+        self.conv= nn.Conv1d(args.num_channels, out_channels=32, kernel_size=5,stride=1)
+        self.conv_pad = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=5,stride=1,padding=2)
+        self.drop_50 = nn.Dropout(p=0.5)
+        self.maxpool = nn.MaxPool1d(kernel_size=5,stride=2) 
+        self.dense1 = nn.Linear(32 * 8, 32) 
+        self.dense2 = nn.Linear(32, 32) 
+        self.dense_final = nn.Linear(32, 5)
+        self.softmax= nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, 1)
+        
+        residual= self.conv(x)
+      
+        #block1 
+        x = F.relu(self.conv_pad(residual))
+        x = self.conv_pad(x)
+        x+= residual 
+        x = F.relu(x)
+        residual = self.maxpool(x) #[512 32 90]
+       
+        #block2
+        x=F.relu(self.conv_pad(residual))
+        x=self.conv_pad(x)
+        x+=residual
+        x= F.relu(x)
+        residual = self.maxpool(x) #[512 32 43]
+        
+        
+        #block3
+        x=F.relu(self.conv_pad(residual))
+        x=self.conv_pad(x)
+        x+=residual
+        x= F.relu(x)
+        residual = self.maxpool(x) #[512 32 20]
+        
+        
+        #block4
+        x=F.relu(self.conv_pad(residual))
+        x=self.conv_pad(x)
+        x+=residual
+        x= F.relu(x)
+        x= self.maxpool(x) #[512 32 8]
+        
+        #MLP
+        x = x.view(-1, 32 * 8) #Reshape (current_dim, 32*2)
+        x = F.relu(self.dense1(x))
+        #x = self.drop_60(x)
+        x= self.dense2(x)
+        x = self.softmax(self.dense_final(x))
+        return x
+
--- models/Update.py
+++ models/Update.py
@@ -23,12 +23,31 @@ class DatasetSplit(Dataset):
         return image, label
 
 
+
+class DatasetSplitH(Dataset):
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = list(idxs)
+
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image = self.dataset[self.idxs[item]][0]
+        label = self.dataset[self.idxs[item]][1]
+        return image, label
+
+
 class LocalUpdate(object):
     def __init__(self, args, dataset=None, idxs=None):
         self.args = args
         self.loss_func = nn.CrossEntropyLoss()
         self.selected_clients = []
-        self.ldr_train = DataLoader(DatasetSplit(dataset, idxs), batch_size=self.args.local_bs, shuffle=True)
+        if self.args.dataset == 'Heartbeat': # Modified Heart
+            self.ldr_train = DataLoader(DatasetSplitH(dataset, idxs), batch_size=self.args.local_bs, shuffle=True) # Modified Heart
+        else: # Modified Heart
+            self.ldr_train = DataLoader(DatasetSplit(dataset, idxs), batch_size=self.args.local_bs, shuffle=True) # Modified Heart
 
     def train(self, net):
         net.train()
@@ -36,13 +55,18 @@ class LocalUpdate(object):
         optimizer = torch.optim.SGD(net.parameters(), lr=self.args.lr, momentum=self.args.momentum)
 
         epoch_loss = []
+        epoch_acc = []
         for iter in range(self.args.local_ep):
             batch_loss = []
+            batch_acc = []
             for batch_idx, (images, labels) in enumerate(self.ldr_train):
                 images, labels = images.to(self.args.device), labels.to(self.args.device)
+                correct111 = 0 # Modified
                 net.zero_grad()
                 log_probs = net(images)
                 loss = self.loss_func(log_probs, labels)
+                y_pred = log_probs.data.max(1, keepdim=True)[1] # Modified
+                correct111 += y_pred.eq(labels.data.view_as(y_pred)).long().cpu().sum() # Modified	
                 loss.backward()
                 optimizer.step()
                 if self.args.verbose and batch_idx % 10 == 0:
@@ -50,6 +74,9 @@ class LocalUpdate(object):
                         iter, batch_idx * len(images), len(self.ldr_train.dataset),
                                100. * batch_idx / len(self.ldr_train), loss.item()))
                 batch_loss.append(loss.item())
+                batch_acc.append(100.00 * correct111 / len(images))
             epoch_loss.append(sum(batch_loss)/len(batch_loss))
-        return net.state_dict(), sum(epoch_loss) / len(epoch_loss)
+            epoch_acc.append(sum(batch_acc)/len(batch_acc))
+        #print(correct111, len(images), len(self.ldr_train), len(self.ldr_train.dataset))
+        return net.state_dict(), sum(epoch_loss) / len(epoch_loss), sum(epoch_acc) / len(epoch_acc) # Modified
 
